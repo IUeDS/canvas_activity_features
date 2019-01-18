1651533fee113201d8c401bc40cdb72aa6947aef
Ben Motz <bmotz@indiana.edu>
2019-01-18 15:36:15 -0500

    Update 04_avgRequests.sql

--- requests/04_avgRequests.sql
+++ requests/04_avgRequests.sql
@@ -1,34 +1,56 @@
 /* Computes average number of requests (proxy for number of page views) in which an enrollment accessed an assignment */
 WITH
-  numAssnRequests AS ( --subquery calculates number of reqeusts by an enrollment within a section
+  theseRequests AS (
   SELECT
-    course_id,
+    id,
     user_id,
-    session_id,
-    COUNT(id) AS AssnRequests
+    REGEXP_EXTRACT(url,r'assignments\/(\d+)') AS assignment_id,
+    session_id AS this_session,
+    LEAD(session_id) OVER (ORDER BY user_id, timestamp) AS next_session,
+    url,
+    course_id,
+    web_application_action,
+    web_application_controller
   FROM
     canvas_data.requests
-  WHERE
-    timestamp_day BETWEEN '2018-08-20'
+  WHERE timestamp_day BETWEEN '2018-08-20'
     AND '2018-09-10'
-    AND course_id IS NOT NULL
-    AND user_id IS NOT NULL
-    AND web_application_controller='assignments'
-    AND NOT (url LIKE '%assignments/syllabys%/')
+  ORDER BY
+    user_id,
+    timestamp ),
+  sessionsWithAssignmentViews AS (
+  SELECT
+    DISTINCT this_session
+  FROM
+    theseRequests
+  WHERE
+    web_application_controller = 'assignments'
+    AND NOT (url LIKE '%assignments/syllabus%')),
+  viewsPerSession AS (
+  SELECT
+    COUNT(a.id) AS cntRequests,
+    a.user_id,
+    a.course_id,
+    b.this_session
+  FROM
+    theseRequests a
+  JOIN
+    sessionsWithAssignmentViews b
+  ON
+    a.this_session = b.this_session
+  WHERE
+    a.this_session = a.next_session
     AND web_application_action = 'show'
   GROUP BY
-    course_id,
-    user_id,
-    session_id
-  ORDER BY
-    course_id,
-    user_id)
+    a.user_id,
+    a.course_id,
+    b.this_session )
 SELECT
-  course_id,
   user_id,
-  AVG(AssnRequests) AS avgAssnRequests
-FROM
-  numAssnRequests
-GROUP BY
   course_id,
-  user_id
+  AVG(cntRequests) AS numberOfRequests
+FROM
+  viewsPerSession
+GROUP BY
+  user_id,
+  course_id
