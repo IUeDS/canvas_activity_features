9508132f15fd41a760bf0d78b1243dcbb9e2db51
Benjamin Motz <bmotz@indiana.edu>
2019-05-13 14:03:44 -0400

    Create 18_numberOfSessions.sql

---
+++ events/18_numberOfSessions.sql
@@ -0,0 +1,15 @@
+/* Counts number of sessions per enrollment */
+SELECT
+  SUBSTR(JSON_EXTRACT_SCALAR(event,"$[group][extensions]['com.instructure.canvas'][entity_id]"),-7,7) AS course_id,
+  SUBSTR(JSON_EXTRACT_SCALAR(event,"$[actor][id]"),-7,7) AS user_id,
+  COUNT(DISTINCT SUBSTR(JSON_EXTRACT_SCALAR(event,"$[session][id]"),-32,32)) AS numberOfSessions
+FROM
+  event_store.events
+WHERE
+  /* event_time BETWEEN '2018-08-20' AND '2018-09-10' */
+  SUBSTR(JSON_EXTRACT_SCALAR(event,"$[group][extensions]['com.instructure.canvas'][entity_id]"),-7,7) IS NOT NULL 
+  AND SUBSTR(JSON_EXTRACT_SCALAR(event,"$[actor][id]"),-7,7) IS NOT NULL
+  AND JSON_EXTRACT_SCALAR(event,"$[actor][type]") = "Person"
+GROUP BY
+  SUBSTR(JSON_EXTRACT_SCALAR(event,"$[group][extensions]['com.instructure.canvas'][entity_id]"),-7,7),
+  SUBSTR(JSON_EXTRACT_SCALAR(event,"$[actor][id]"),-7,7)
